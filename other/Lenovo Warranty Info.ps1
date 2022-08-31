0cb64dedffd1484defc71932f371890f4f1e83fd
Tanner Smallwood <smallwood.tanner23@gmail.com>
2022-08-31 14:58:51 -0600

    Removing scrips that were not written by me.

--- other/Lenovo Warranty Info.ps1
+++
@@ -1,5 +0,0 @@
-﻿Get-CimInstance -Namespace root/Lenovo -ClassName Lenovo_WarrantyInformation | Select-Object `
-    SerialNumber, `
-    StartDate, `
-    EndDate, `
-    LastUpdateTime | ConvertTo-Json
